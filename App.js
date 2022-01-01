175c4f33fe0a198d828f9551f352323ead41abcc
nesrinmohamed <94196690+nesrinmohamed@users.noreply.github.com>
2022-01-01 11:02:28 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoList from "./views/TodoList";
+import Header from "./components/Header";
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    <TodoList />
+    </div>
+  );
+}
+
+export default App;
